d77c95b349f89d9ae9617214c7de7e2a1d36c8fe
gau-tam-byte <gautamkumark1214@gmail.com>
2023-06-24 12:49:58 -0400

    new finalfinisehs

--- final_project/machinetranslation/test.py
+++ final_project/machinetranslation/test.py
@@ -5,10 +5,13 @@ from translator import englishtofrench, frenchtoenglish
 class Testetf(unittest.TestCase):
     def test1 (self):
         self.assertNotEqual(englishtofrench("Hello"), "Bonjour")
+        self.assertEqual(englishtofrench("Hi"), "Bonjour")
 
 
 class Testfte(unittest.TestCase):
-    def test1(self):
+    def test2(self):
         self.assertEqual(frenchtoenglish("bonjour comment"), "hello how")
+        self.assertNotEqual(frenchtoenglish("Salut"), "Hi")
     
-unittest.main()
+if __name__ == "__main__":
+    unittest.main()
